4f0099b8263a837f975ebd0a90b605e30a47ebb0
Ptah Dunbar <yarr@piratedunbar.com>
2015-06-17 16:35:34 -0400

    First step towards making contact.php dynamic

--- web/footer.php
+++ web/footer.php
@@ -26,6 +26,13 @@
 	})
 </script>
 
+<?php if ( '/contact.php' == getCurrentPage() ) : ?>
+	<!-- Contact Form JavaScript -->
+	<!-- Do not edit these files! In order to set the email address and subject line for the contact form go to the bin/contact_me.php file. -->
+	<script src="js/jqBootstrapValidation.js"></script>
+	<script src="js/contact_me.js"></script>
+<?php endif; ?>
+
 </body>
 
 </html>
