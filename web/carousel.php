8b19bdd5496016b59ff981df9a9c4765bcdab6a3
Ptah Dunbar <yarr@piratedunbar.com>
2015-06-17 16:34:32 -0400

    finish making the carousel dynamic

--- web/carousel.php
+++ web/carousel.php
@@ -1,27 +1,24 @@
 <?php require_once 'bootstrap.php'; ?>
+<?php $carousel = getCarousel(getCurrentPage()); ?>
 <!-- Header Carousel -->
 <header id="myCarousel" class="carousel slide">
 	<!-- Indicators -->
 	<ol class="carousel-indicators">
-		<?php for ($i = 0; $i < count($about_carousel); $i++ ) : ?>
-			<li data-target="#myCarousel" data-slide-to="<?= $i; ?>" <?= ($i == 0) ? 'class="active"' : ''; ?>></li>
+		<?php $carousel_length = count($carousel) - 1;  ?>
+		<?php for ($index = $carousel_length ; $index >= 0; $index--) : ?>
+			<li data-target="#myCarousel" data-slide-to="<?= $index; ?>" <?= ($index == $carousel_length) ? 'class="active"' : ''; ?>></li>
 		<?php endfor; ?>
 	</ol>
-
 	<!-- Wrapper for slides -->
 	<div class="carousel-inner">
-		<div class="item">
-			<div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Two');"></div>
-			<div class="carousel-caption">
-				<h2>Caption 2</h2>
+		<?php for ( $index = $carousel_length ; $index >= 0; $index-- ) : ?>
+			<div class="item<?= ($index == $carousel_length) ? ' active' : ''; ?>">
+				<div class="fill" style="background-image:url('<?= $carousel[$index]['img_url'] ?>');"></div>
+				<div class="carousel-caption">
+					<h2><?= $carousel[$index]['caption'] ?></h2>
+				</div>
 			</div>
-		</div>
-		<div class="item">
-			<div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide Three');"></div>
-			<div class="carousel-caption">
-				<h2>Caption 3</h2>
-			</div>
-		</div>
+		<?php endfor; ?>
 	</div>
 
 	<!-- Controls -->
